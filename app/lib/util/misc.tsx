dbc02a39e793104ae9e69c9001563b4018c4dfcb
Ro Oeurn <ro@nowopen.studio>
2024-11-22 19:04:08 -0800

    new int

--- app/lib/util/misc.tsx
+++ app/lib/util/misc.tsx
@@ -4,7 +4,7 @@
     let full:any = [];
     var newText = (text!).split('');
     for (var i in newText) {
-        var curr:any = <span key={`toolLetter-${i}`} className="toolLetter" style={{animationDelay:`${i*.015 + num!}s`}}>{newText[i]}</span>;
+        var curr:any = <span key={`toolLetter-${i}`} className="toolLetter" style={{animationDelay:`${ Number(i)*.015 + num!}s`}}>{newText[i]}</span>;
         full.push(curr)
       }
 
